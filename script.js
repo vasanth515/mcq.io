a5077dc51d16f9b4bdf3f23b5c58b0f1c6971450
vasanth <75570997+vasanth515@users.noreply.github.com>
2022-05-17 16:46:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,357 @@
+function highlight1(){
+ document.getElementById('highlight1').style.color = "blueviolet";
+ document.getElementById('highlight2').style.color = "black";
+ document.getElementById('highlight3').style.color = "black";
+ document.getElementById('highlight4').style.color = "black";
+}
+
+function highlight2(){
+ document.getElementById('highlight1').style.color = "black";
+ document.getElementById('highlight2').style.color = "blueviolet";
+ document.getElementById('highlight3').style.color = "black";
+ document.getElementById('highlight4').style.color = "black";
+}
+
+
+function highlight3(){
+ document.getElementById('highlight1').style.color = "black";
+ document.getElementById('highlight2').style.color = "black";
+ document.getElementById('highlight3').style.color = "blueviolet";
+ document.getElementById('highlight4').style.color = "black";
+}
+
+
+function highlight4(){
+ document.getElementById('highlight1').style.color = "black";
+ document.getElementById('highlight2').style.color = "black";
+ document.getElementById('highlight3').style.color = "black";
+ document.getElementById('highlight4').style.color = "blueviolet";
+}
+
+function highlight5(){
+ document.getElementById('highlight5').style.color = "blueviolet";
+ document.getElementById('highlight6').style.color = "black";
+ document.getElementById('highlight7').style.color = "black";
+ document.getElementById('highlight8').style.color = "black";
+}
+
+function highlight6(){
+ document.getElementById('highlight5').style.color = "black";
+ document.getElementById('highlight6').style.color = "blueviolet";
+ document.getElementById('highlight7').style.color = "black";
+ document.getElementById('highlight8').style.color = "black";
+}
+
+
+function highlight7(){
+ document.getElementById('highlight5').style.color = "black";
+ document.getElementById('highlight6').style.color = "black";
+ document.getElementById('highlight7').style.color = "blueviolet";
+ document.getElementById('highlight8').style.color = "black";
+}
+
+
+function highlight8(){
+ document.getElementById('highlight5').style.color = "black";
+ document.getElementById('highlight6').style.color = "black";
+ document.getElementById('highlight7').style.color = "black";
+ document.getElementById('highlight8').style.color = "blueviolet";
+}
+
+
+function highlight9(){
+ document.getElementById('highlight9').style.color = "blueviolet";
+ document.getElementById('highlight10').style.color = "black";
+ document.getElementById('highlight11').style.color = "black";
+ document.getElementById('highlight12').style.color = "black";
+}
+
+function highlight10(){
+ document.getElementById('highlight9').style.color = "black";
+ document.getElementById('highlight10').style.color = "blueviolet";
+ document.getElementById('highlight11').style.color = "black";
+ document.getElementById('highlight12').style.color = "black";
+}
+
+
+function highlight11(){
+ document.getElementById('highlight9').style.color = "black";
+ document.getElementById('highlight10').style.color = "black";
+ document.getElementById('highlight11').style.color = "blueviolet";
+ document.getElementById('highlight12').style.color = "black";
+}
+
+
+function highlight12(){
+ document.getElementById('highlight9').style.color = "black";
+ document.getElementById('highlight10').style.color = "black";
+ document.getElementById('highlight11').style.color = "black";
+ document.getElementById('highlight12').style.color = "blueviolet";
+}
+
+function highlight13(){
+ document.getElementById('highlight13').style.color = "blueviolet";
+ document.getElementById('highlight14').style.color = "black";
+ document.getElementById('highlight15').style.color = "black";
+ document.getElementById('highlight16').style.color = "black";
+}
+
+function highlight14(){
+ document.getElementById('highlight13').style.color = "black";
+ document.getElementById('highlight14').style.color = "blueviolet";
+ document.getElementById('highlight15').style.color = "black";
+ document.getElementById('highlight16').style.color = "black";
+}
+
+
+function highlight15(){
+ document.getElementById('highlight13').style.color = "black";
+ document.getElementById('highlight14').style.color = "black";
+ document.getElementById('highlight15').style.color = "blueviolet";
+ document.getElementById('highlight16').style.color = "black";
+}
+
+
+function highlight16(){
+ document.getElementById('highlight13').style.color = "black";
+ document.getElementById('highlight14').style.color = "black";
+ document.getElementById('highlight15').style.color = "black";
+ document.getElementById('highlight16').style.color = "blueviolet";
+}
+
+
+function highlight17(){
+ document.getElementById('highlight17').style.color = "blueviolet";
+ document.getElementById('highlight18').style.color = "black";
+ document.getElementById('highlight19').style.color = "black";
+ document.getElementById('highlight20').style.color = "black";
+}
+
+function highlight18(){
+ document.getElementById('highlight17').style.color = "black";
+ document.getElementById('highlight18').style.color = "blueviolet";
+ document.getElementById('highlight19').style.color = "black";
+ document.getElementById('highlight20').style.color = "black";
+}
+
+
+function highlight19(){
+ document.getElementById('highlight17').style.color = "black";
+ document.getElementById('highlight18').style.color = "black";
+ document.getElementById('highlight19').style.color = "blueviolet";
+ document.getElementById('highlight20').style.color = "black";
+}
+
+
+function highlight20(){
+ document.getElementById('highlight17').style.color = "black";
+ document.getElementById('highlight18').style.color = "black";
+ document.getElementById('highlight19').style.color = "black";
+ document.getElementById('highlight20').style.color = "blueviolet";
+}
+
+function highlight21(){
+ document.getElementById('highlight21').style.color = "blueviolet";
+ document.getElementById('highlight22').style.color = "black";
+ document.getElementById('highlight23').style.color = "black";
+ document.getElementById('highlight24').style.color = "black";
+}
+
+function highlight22(){
+ document.getElementById('highlight21').style.color = "black";
+ document.getElementById('highlight22').style.color = "blueviolet";
+ document.getElementById('highlight23').style.color = "black";
+ document.getElementById('highlight24').style.color = "black";
+}
+
+
+function highlight23(){
+ document.getElementById('highlight21').style.color = "black";
+ document.getElementById('highlight22').style.color = "black";
+ document.getElementById('highlight23').style.color = "blueviolet";
+ document.getElementById('highlight24').style.color = "black";
+}
+
+
+function highlight24(){
+ document.getElementById('highlight21').style.color = "black";
+ document.getElementById('highlight22').style.color = "black";
+ document.getElementById('highlight23').style.color = "black";
+ document.getElementById('highlight24').style.color = "blueviolet";
+}
+
+
+function highlight25(){
+ document.getElementById('highlight25').style.color = "blueviolet";
+ document.getElementById('highlight26').style.color = "black";
+ document.getElementById('highlight27').style.color = "black";
+ document.getElementById('highlight28').style.color = "black";
+}
+
+function highlight26(){
+ document.getElementById('highlight25').style.color = "black";
+ document.getElementById('highlight26').style.color = "blueviolet";
+ document.getElementById('highlight27').style.color = "black";
+ document.getElementById('highlight28').style.color = "black";
+}
+
+
+function highlight27(){
+ document.getElementById('highlight25').style.color = "black";
+ document.getElementById('highlight26').style.color = "black";
+ document.getElementById('highlight27').style.color = "blueviolet";
+ document.getElementById('highlight28').style.color = "black";
+}
+
+
+function highlight28(){
+ document.getElementById('highlight25').style.color = "black";
+ document.getElementById('highlight26').style.color = "black";
+ document.getElementById('highlight27').style.color = "black";
+ document.getElementById('highlight28').style.color = "blueviolet";
+}
+
+function highlight29(){
+ document.getElementById('highlight29').style.color = "blueviolet";
+ document.getElementById('highlight30').style.color = "black";
+ document.getElementById('highlight31').style.color = "black";
+ document.getElementById('highlight32').style.color = "black";
+}
+
+function highlight30(){
+ document.getElementById('highlight29').style.color = "black";
+ document.getElementById('highlight30').style.color = "blueviolet";
+ document.getElementById('highlight31').style.color = "black";
+ document.getElementById('highlight32').style.color = "black";
+}
+
+
+function highlight31(){
+ document.getElementById('highlight29').style.color = "black";
+ document.getElementById('highlight30').style.color = "black";
+ document.getElementById('highlight31').style.color = "blueviolet";
+ document.getElementById('highlight32').style.color = "black";
+}
+
+
+function highlight32(){
+ document.getElementById('highlight29').style.color = "black";
+ document.getElementById('highlight30').style.color = "black";
+ document.getElementById('highlight31').style.color = "black";
+ document.getElementById('highlight32').style.color = "blueviolet";
+}
+
+function highlight33(){
+ document.getElementById('highlight33').style.color = "blueviolet";
+ document.getElementById('highlight34').style.color = "black";
+ document.getElementById('highlight35').style.color = "black";
+ document.getElementById('highlight36').style.color = "black";
+}
+
+function highlight34(){
+ document.getElementById('highlight33').style.color = "black";
+ document.getElementById('highlight34').style.color = "blueviolet";
+ document.getElementById('highlight35').style.color = "black";
+ document.getElementById('highlight36').style.color = "black";
+}
+
+
+function highlight35(){
+ document.getElementById('highlight33').style.color = "black";
+ document.getElementById('highlight34').style.color = "black";
+ document.getElementById('highlight35').style.color = "blueviolet";
+ document.getElementById('highlight36').style.color = "black";
+}
+
+
+function highlight36(){
+ document.getElementById('highlight33').style.color = "black";
+ document.getElementById('highlight34').style.color = "black";
+ document.getElementById('highlight35').style.color = "black";
+ document.getElementById('highlight36').style.color = "blueviolet";
+}
+
+function highlight37(){
+ document.getElementById('highlight37').style.color = "blueviolet";
+ document.getElementById('highlight38').style.color = "black";
+ document.getElementById('highlight39').style.color = "black";
+ document.getElementById('highlight40').style.color = "black";
+}
+
+function highlight38(){
+ document.getElementById('highlight37').style.color = "black";
+ document.getElementById('highlight38').style.color = "blueviolet";
+ document.getElementById('highlight39').style.color = "black";
+ document.getElementById('highlight40').style.color = "black";
+}
+
+
+function highlight39(){
+ document.getElementById('highlight37').style.color = "black";
+ document.getElementById('highlight38').style.color = "black";
+ document.getElementById('highlight39').style.color = "blueviolet";
+ document.getElementById('highlight40').style.color = "black";
+}
+
+
+function highlight40(){
+ document.getElementById('highlight37').style.color = "black";
+ document.getElementById('highlight38').style.color = "black";
+ document.getElementById('highlight39').style.color = "black";
+ document.getElementById('highlight40').style.color = "blueviolet";
+}
+
+
+
+
+function submit(){
+	document.getElementById('highlight1').style.color = "red";
+	document.getElementById('highlight2').style.color = "red";
+	document.getElementById('highlight3').style.color = "green";
+	document.getElementById('highlight4').style.color = "red";
+
+	document.getElementById('highlight5').style.color = "red";
+	document.getElementById('highlight6').style.color = "red";
+	document.getElementById('highlight7').style.color = "green";
+	document.getElementById('highlight8').style.color = "red";
+
+	document.getElementById('highlight9').style.color = "green";
+	document.getElementById('highlight10').style.color = "red";
+	document.getElementById('highlight11').style.color = "red";
+	document.getElementById('highlight12').style.color = "red";
+
+	document.getElementById('highlight13').style.color = "red";
+	document.getElementById('highlight14').style.color = "green";
+	document.getElementById('highlight15').style.color = "red";
+	document.getElementById('highlight16').style.color = "red";
+
+	document.getElementById('highlight17').style.color = "red";
+	document.getElementById('highlight18').style.color = "red";
+	document.getElementById('highlight19').style.color = "green";
+	document.getElementById('highlight20').style.color = "red";
+
+	document.getElementById('highlight21').style.color = "red";
+	document.getElementById('highlight22').style.color = "red";
+	document.getElementById('highlight23').style.color = "green";
+	document.getElementById('highlight24').style.color = "red";
+
+	document.getElementById('highlight25').style.color = "red";
+	document.getElementById('highlight26').style.color = "green";
+	document.getElementById('highlight27').style.color = "red";
+	document.getElementById('highlight28').style.color = "red";
+
+	document.getElementById('highlight29').style.color = "red";
+	document.getElementById('highlight30').style.color = "red";
+	document.getElementById('highlight31').style.color = "red";
+	document.getElementById('highlight32').style.color = "green";
+
+	document.getElementById('highlight33').style.color = "green";
+	document.getElementById('highlight34').style.color = "red";
+	document.getElementById('highlight35').style.color = "red";
+	document.getElementById('highlight36').style.color = "red";
+
+	document.getElementById('highlight37').style.color = "red";
+	document.getElementById('highlight38').style.color = "red";
+	document.getElementById('highlight39').style.color = "green";
+	document.getElementById('highlight40').style.color = "red";
+}
